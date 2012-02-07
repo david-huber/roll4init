9c5559cc1cddd3fcd07bd93421978767da35e4ac
David Huber <david.huber@gmail.com>
2012-02-06 18:37:59 -0600

    implementing put and post

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,6 +1,7 @@
-from flask.helpers import jsonify, url_for
-
+from dice.dice_parser import DiceParser
+from flask.helpers import jsonify, url_for, request
 import pymongo
+from tracker.fighter import Fighter
 
 def add_fighter_routes(app, g):
 
@@ -10,7 +11,8 @@ def add_fighter_routes(app, g):
     def get_fighters(page):
         if page < 1: page = 0
 
-        return jsonify(fighters=g.fighterRepository.find(skip=(page-1)*100, limit=100, sort=[('name', pymongo.ASCENDING)]),
+        return jsonify(fighters=g.fighterRepository.find(
+            skip=(page-1)*100, limit=100, sort=[('name', pymongo.ASCENDING)]),
             next=url_for("fighters", page=page+1))
 
 
@@ -18,10 +20,38 @@ def add_fighter_routes(app, g):
     def get_fighter(fighter_id):
         return jsonify(fighter=g.fighterRepository.find_one(_id=fighter_id))
 
+    def get_initiative():
+        dice_parser = DiceParser()
+        initiative = dice_parser.parse(request.form["initiative"])
+        return initiative
+
+    def save_fighter(fighter):
+        g.fighterRepository.save(fighter)
+        return jsonify(success=False)
+
     @app.route("/fighter/<fighter_id>", methods=["PUT"])
     def put_fighter(fighter_id):
-        pass
+        initiative = None
+        try:
+            initiative = get_initiative()
+        except (ValueError):
+            return jsonify(success=False)
+
+        fighter = Fighter(id= fighter_id,
+            name=request.form["name"],
+            initiative=initiative)
+
+        return save_fighter(fighter)
 
     @app.route("/fighter", methods=["POST"])
     def post_fighter():
-        pass
+        initiative = None
+        try:
+            initiative = get_initiative()
+        except (ValueError):
+            return jsonify(success=False)
+
+        fighter = Fighter(name=request.form["name"],
+            initiative=initiative)
+
+        return save_fighter(fighter)
